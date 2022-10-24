36ee18a079988e5652556d34670ab673d888d411
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-24 19:26:02 +0530

    adding program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace vendingmachined6
+{
+    internal class program
+    {
+        public static void main(string[] args)
+        {
+            Console.WriteLine("vending machine");
+        }
+    }
+}
